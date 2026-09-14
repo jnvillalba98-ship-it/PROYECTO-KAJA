bdb53428b1b16559ed69786970bb04040d676036
Nicolás <jnvillalba98@gmail.com>
2026-09-13 23:41:33 -0500

    Ajustar rutas y CORS en server.js

--- kaja-server/server.js
+++ kaja-server/server.js
@@ -23,7 +23,7 @@ app.use(express.json({ limit: '1mb' }));
 app.use('/KAJA-FRONTED', express.static(path.join(__dirname, 'KAJA-FRONTED')));
 app.use('/LOGIN-KAJA', express.static(path.join(__dirname, 'LOGIN-KAJA')));
 
-app.use('/api/auth', authRoutes);
+app.use('/api', authRoutes);
 app.use('/api/empresas', empresaRoutes);
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/roles', rolRoutes);
